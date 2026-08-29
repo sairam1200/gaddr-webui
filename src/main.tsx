cff9d6b0a8afd1e8b20898144896e9b46883c598
Team Gaddr <team@gaddr.com>
2026-08-29 12:31:20 +0530

    Apply Gaddr purple interface theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 /*
  * SPDX-FileCopyrightText: 2020 Stalwart Labs LLC <hello@stalw.art>
+ * SPDX-FileCopyrightText: 2026 Gaddr
  *
  * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-SEL
  */
@@ -17,6 +18,8 @@ import { AdminPanel } from './pages/AdminPanel.lazy';
 import { ProtectedRoute } from './components/layout/ProtectedRoute';
 import { getBasePath } from './lib/basePath';
 import { loadLogoOnce } from './lib/logoCache';
+
+document.documentElement.dataset.gaddrBuild = '20260829';
 
 (() => {
   try {
